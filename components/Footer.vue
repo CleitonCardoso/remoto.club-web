c3864f552fc228b760fe05b9d18bc37f27d31256
Cleiton Cardoso <cleitoncardoso_joy@hotmail.com>
2020-11-06 15:41:49 -0300

    ajuste footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -6,7 +6,7 @@
         style="text-decoration: none; color: inherit;"
         target="_blank"
         rel="noopener noreferrer"
-        >Desenvolvido por Cleiton Cardoso - {{ new Date().getFullYear() }}</a
+        >Remoto.Club - {{ new Date().getFullYear() }}</a
       >
     </v-flex>
   </v-footer>
