543502c0bc79d0a3beef11c30e93f033119f1afe
Raffsx <rafaeldeagostinho@gmail.com>
2021-02-12 21:32:23 -0300

    Iniciando chat

--- pages/my-applications/_uuid.vue
+++ pages/my-applications/_uuid.vue
@@ -1,209 +1,245 @@
 <template>
-  <v-flex class="align-start">
-    <v-card dark color="black darken-1">
-      <v-card-title>
-        <div>
-          <div>
-            <strong>
+<v-flex class="align-start">
+<v-card>
+<v-card-title style="cursor: pointer;" @click="revealer()">
+<div>
+<div>
+<strong>
               {{ job.title }} -
-              {{ job.contractType }}
-            </strong>
-          </div>
-          {{ job.company }}
-          <span v-if="job.officeLocation"> | {{ job.officeLocation }}</span>
-        </div>
-        <v-spacer></v-spacer>
-        <div>
-          <div>
+                      {{ job.contractType }}
+</strong>
+</div>
+        {{ job.company }}
+<span v-if="job.officeLocation"> | {{ job.officeLocation }}</span>
+</div>
+<v-spacer></v-spacer>
+<div>
+<div>
             Nível:
-            {{ job.experienceRequired }}
-          </div>
-          <strong>
+                    {{ job.experienceRequired }}
+</div>
+<strong>
             {{
-              format(job.salary) + '/' + getCompensationTypeLabel(job)
-            }}</strong
-          >
-        </div>
-      </v-card-title>
-
-      <v-card-text>
-        <div class="my-4">
-          <p style="white-space: pre-wrap;">
-            {{ job.description }}
-          </p>
-        </div>
-      </v-card-text>
-    </v-card>
-    <v-stepper v-model="e1">
-      <v-stepper-header v-if="application" dark>
-        <v-stepper-step step="1" complete>
-          Você se aplicou
-        </v-stepper-step>
-
-        <v-divider></v-divider>
-
-        <v-stepper-step
-          :complete="
-            ['ACCEPTED', 'OFFERED', 'HIRED'].includes(application.status)
-          "
-          step="2"
+                    format(job.salary) + '/' + getCompensationTypeLabel(job)
+                    }}</strong
         >
-          A empresa quer te conhecer
-        </v-stepper-step>
-
-        <v-divider></v-divider>
-
-        <v-stepper-step
-          :complete="['OFFERED', 'HIRED'].includes(application.status)"
-          step="3"
+</div>
+</v-card-title>
+<v-expand-transition>
+<v-card v-show="reveal">
+<v-container
+        style="white-space: pre-wrap; cursor: pointer;"
+@click="revealer()"
         >
-          Você recebeu uma oferta
-        </v-stepper-step>
+        {{ job.description }}
+</v-container>
+</v-card>
+</v-expand-transition>
+</v-card>
+<v-stepper v-model="e1">
+<v-stepper-header v-if="application" dark>
+<v-stepper-step step="1" complete>
+        Você se aplicou
+</v-stepper-step>
 
-        <v-divider></v-divider>
+<v-divider></v-divider>
 
-        <v-stepper-step :complete="application.status === 'HIRED'" step="4">
-          Contratado!
-        </v-stepper-step>
-      </v-stepper-header>
+<v-stepper-step
+        :complete="
+        ['ACCEPTED', 'OFFERED', 'HIRED'].includes(application.status)
+        "
+        step="2"
+        >
+        A empresa quer te conhecer
+</v-stepper-step>
 
-      <v-stepper-items>
-        <v-stepper-content step="1">
-          <v-card height="200px"> </v-card>
-        </v-stepper-content>
+<v-divider></v-divider>
 
-        <v-stepper-content step="2">
-          <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+<v-stepper-step
+        :complete="['OFFERED', 'HIRED'].includes(application.status)"
+        step="3"
+        >
+        Você recebeu uma oferta
+</v-stepper-step>
 
-          <v-btn @click="e1 = 3">
-            Continue
-          </v-btn>
+<v-divider></v-divider>
 
-          <v-btn text>
-            Cancel
-          </v-btn>
-        </v-stepper-content>
+<v-stepper-step :complete="application.status === 'HIRED'" step="4">
+        Contratado!
+</v-stepper-step>
+</v-stepper-header>
 
-        <v-stepper-content step="3">
-          <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+<v-stepper-items>
+<v-stepper-content step="1"> </v-stepper-content>
 
-          <v-btn color="primary" @click="e1 = 1">
-            Continue
-          </v-btn>
+<v-stepper-content step="2">
+<v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
 
-          <v-btn text>
-            Cancel
-          </v-btn>
-        </v-stepper-content>
-      </v-stepper-items>
-    </v-stepper>
-  </v-flex>
+<v-btn @click="e1 = 3">
+        Continue
+</v-btn>
+
+<v-btn text>
+        Cancel
+</v-btn>
+</v-stepper-content>
+
+<v-stepper-content step="3">
+<v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+
+<v-btn color="primary" @click="e1 = 1">
+        Continue
+</v-btn>
+
+<v-btn text>
+        Cancel
+</v-btn>
+</v-stepper-content>
+</v-stepper-items>
+</v-stepper>
+<v-card
+        ><template>
+<div>
+<v-row justify="center" align="center">
+<v-subheader>{Mensagens}:</v-subheader>
+        {{ messages.length }}
+</v-row>
+<v-container
+        id="scroll-target"
+        style="max-height: 400px;"
+class="overflow-y-auto"
+        >
+<v-row
+        v-scroll:#scroll-target="onScroll"
+        align="center"
+        justify="center"
+        style="height: 150px;"
+        >
+</v-row>
+<v-alert
+        dense
+        outlined
+        elevation="5"
+        border="left"
+        :color="colorMessage"
+        width="20%"
+        >
+        {{ messages[0] }}
+</v-alert>
+<v-alert
+        dense
+        outlined
+        border="right"
+        elevation="10"
+        width="30%"
+        :color="colorMessage"
+        :type="vizualized"
+        >
+        {{ messages[1] }}
+</v-alert>
+</v-container>
+<v-container fluid>
+<v-textarea
+        name="input-7-1"
+        filled
+        label="Digite sua mensagem:"
+        auto-grow
+        value="..."
+        ></v-textarea>
+</v-container>
+</div>
+</template>
+</v-card>
+</v-flex>
 </template>
 <script>
 const brlFormatter = new Intl.NumberFormat('pt-BR', {
-  style: 'currency',
-  currency: 'BRL',
-  maximumFractionDigits: 2,
-  unitDisplay: 'short',
-})
-export default {
-  layout: 'search',
-  middleware: 'auth',
-  data() {
-    return {
-      e1: 1,
-      candidature: null,
-
-      application: [],
-      job: {
-        company: '',
-        compensationType: '',
-        contractType: '',
-        createdDate: '',
-        description: '',
-        experienceRequired: '',
-        jobStatus: '',
-        lastUpdate: '',
-        officeLocation: '',
-        salary: '',
-        title: '',
-        uuid: '',
-      },
-    }
-  },
-  mounted() {
-    this.load()
-  },
-  methods: {
-    format(value) {
-      return brlFormatter.format(value)
-    },
-    getCompensationTypeLabel(job) {
-      if (job.compensationType) {
-        switch (job.compensationType) {
-          case 'PER_HOUR':
-            return 'hora'
-          case 'PER_MONTH':
-            return 'mês'
-          case 'PER_YEAR':
-            return 'ano'
+        style: 'currency',
+        currency: 'BRL',
+        maximumFractionDigits: 2,
+        unitDisplay: 'short',
+        })
+        export default {
+        layout: 'search',
+        middleware: 'auth',
+        data() {
+        return {
+        e1: 1,
+        candidature: null,
+        application: [],
+        job: {},
+        messages: [],
+        reveal: false,
+        colorMessage: 'green',
+        vizualized: 'success',
+        offsetTop: 0,
         }
-      } else {
+        },
+        mounted() {
+        this.load()
+        },
+        methods: {
+        messageReader() {
+        const arrayMessages = this.application.messages
+        arrayMessages.forEach((m) => {
+        this.messages.push(m)
+        })
+        },
+        onScroll(e) {
+        this.offsetTop = e.target.scrollTop
+        },
+        revealer() {
+        if (this.reveal == false) {
+        this.reveal = true
+        } else {
+        this.reveal = false
+        }
+        },
+        format(value) {
+        return brlFormatter.format(value)
+        },
+        getCompensationTypeLabel(job) {
+        if (job.compensationType) {
+        switch (job.compensationType) {
+        case 'PER_HOUR':
+        return 'hora'
+        case 'PER_MONTH':
+        return 'mês'
+        case 'PER_YEAR':
+        return 'ano'
+        }
+        } else {
         if (job.salary >= 0 && job.salary < 600) {
-          return 'hora'
+        return 'hora'
         }
         if (job.salary >= 600 && job.salary < 30000) {
-          return 'mês'
+        return 'mês'
         }
         if (job.salary >= 30000) {
-          return 'ano'
+        return 'ano'
         }
-      }
-    },
-    load() {
-      const jobUuid = this.$route.params.uuid
-      this.$api
+        }
+        },
+        load() {
+        const jobUuid = this.$route.params.uuid
+        this.$api
         .get(`private/my-applications/` + jobUuid)
         .then((res) => {
-          this.application = res.data
-          this.job = res.data.job
-          console.log(this.application)
-          console.log(this.job)
+        this.application = res.data
+        this.job = res.data.job
         })
         .catch((err) => {
-          let message = 'Houve um erro inesperado.'
-          if (err.response && err.response.status === 400) {
-            message = err.response.data.message
-          }
+        let message = 'Houve um erro inesperado.'
+        if (err.response && err.response.status === 400) {
+        message = err.response.data.message
+        }
 
-          this.notification.title = 'Erro'
-          this.notification.description = message
-          this.notification.type = 'error'
-          this.$refs['message-alert'].showAlert()
+        this.notification.title = 'Erro'
+        this.notification.description = message
+        this.notification.type = 'error'
+        this.$refs['message-alert'].showAlert()
         })
-    },
-  },
-  getCompensationTypeLabel(job) {
-    if (job.compensationType) {
-      switch (job.compensationType) {
-        case 'PER_HOUR':
-          return 'hora'
-        case 'PER_MONTH':
-          return 'mês'
-        case 'PER_YEAR':
-          return 'ano'
-      }
-    } else {
-      if (job.salary >= 0 && job.salary < 600) {
-        return 'hora'
-      }
-      if (job.salary >= 600 && job.salary < 30000) {
-        return 'mês'
-      }
-      if (job.salary >= 30000) {
-        return 'ano'
-      }
-    }
-  },
-}
+        },
+        },
+        }
 </script>
--- pages/my-applications/index.vue
+++ pages/my-applications/index.vue
@@ -1,287 +1,287 @@
 <template>
-  <v-container>
-    <v-layout row wrap>
-      <ErrorAlert ref="message-alert" :notification="notification"></ErrorAlert>
-      <v-flex class="align-start">
-        <v-card dark color="black darken-1">
-          <v-card-title>
-            <v-combobox
-              v-model="filter.keyWords"
-              chips
-              clearable
-              hide-details
-              label="Pesquise por palavras chave"
-              prepend-inner-icon="search"
-              multiple
-              solo
-              append-icon=""
-              @click="expand = true"
-            >
-              <template v-slot:selection="{ attrs, item, select, selected }">
-                <v-chip
-                  v-bind="attrs"
-                  :input-value="selected"
-                  close
-                  @click="select"
-                  @click:close="removeKeyWords(item)"
-                >
-                  <strong>{{ item }}</strong
-                  >&nbsp;
-                </v-chip>
-              </template>
-            </v-combobox>
-          </v-card-title>
-          <v-expand-transition>
-            <v-card-text v-show="expand">
-              <v-row>
-                <v-col cols="12" sm="6">
-                  <v-select
-                    v-model="filter.contractTypes"
-                    :items="contractTypes"
-                    label="Tipo de contrato"
-                    item-text="text"
-                    item-value="value"
-                    dark
-                    multiple
-                    solo
-                    dense
-                  ></v-select>
-                </v-col>
-                <v-col cols="12" sm="6">
-                  <v-select
-                    v-model="filter.experienceTypes"
-                    :items="experienceTypes"
-                    label="Experiência"
-                    item-text="text"
-                    item-value="value"
-                    dark
-                    multiple
-                    solo
-                    dense
-                  ></v-select>
-                </v-col>
-              </v-row>
+<v-container>
+<v-layout row wrap>
+<ErrorAlert ref="message-alert" :notification="notification"></ErrorAlert>
+<v-flex class="align-start">
+<v-card dark color="black darken-1">
+<v-card-title>
+<v-combobox
+		v-model="filter.keyWords"
+		chips
+		clearable
+		hide-details
+		label="Pesquise por palavras chave"
+		prepend-inner-icon="search"
+		multiple
+		solo
+		append-icon=""
+@click="expand = true"
+		>
+<template v-slot:selection="{ attrs, item, select, selected }">
+<v-chip
+		v-bind="attrs"
+		:input-value="selected"
+		close
+@click="select"
+@click:close="removeKeyWords(item)"
+		>
+<strong>{{ item }}</strong
+		>&nbsp;
+</v-chip>
+</template>
+</v-combobox>
+</v-card-title>
+<v-expand-transition>
+<v-card-text v-show="expand">
+<v-row>
+<v-col cols="12" sm="6">
+<v-select
+		v-model="filter.contractTypes"
+		:items="contractTypes"
+		label="Tipo de contrato"
+		item-text="text"
+		item-value="value"
+		dark
+		multiple
+		solo
+		dense
+		></v-select>
+</v-col>
+<v-col cols="12" sm="6">
+<v-select
+		v-model="filter.experienceTypes"
+		:items="experienceTypes"
+		label="Experiência"
+		item-text="text"
+		item-value="value"
+		dark
+		multiple
+		solo
+		dense
+		></v-select>
+</v-col>
+</v-row>
 
-              <v-card-actions>
-                <v-btn dark block grey @click="search">
-                  Pesquisar
-                </v-btn>
-              </v-card-actions>
-            </v-card-text>
-          </v-expand-transition>
-        </v-card>
+<v-card-actions>
+<v-btn dark block grey @click="search">
+		Pesquisar
+</v-btn>
+</v-card-actions>
+</v-card-text>
+</v-expand-transition>
+</v-card>
 
-        <v-divider class="mt-5 mb-5"></v-divider>
+<v-divider class="mt-5 mb-5"></v-divider>
 
-        <v-pagination
-          v-if="this.applications.length > 0"
-          v-model="pageIndex"
-          dark
-          :length="resultSize"
-          @input="nextPage"
-        ></v-pagination>
+<v-pagination
+		v-if="this.applications.length > 0"
+		v-model="pageIndex"
+		dark
+		:length="resultSize"
+@input="nextPage"
+		></v-pagination>
 
-        <div v-if="this.loading" class="text-center ma-5">
-          <v-progress-circular
-            :size="100"
-            :width="10"
-            indeterminate
-            dark
-          ></v-progress-circular>
-        </div>
+<div v-if="this.loading" class="text-center ma-5">
+<v-progress-circular
+		:size="100"
+		:width="10"
+		indeterminate
+		dark
+		></v-progress-circular>
+</div>
 
-        <v-row
-          v-for="(application, index) in applications"
-          :key="`job-${index}`"
-        >
-          <v-col>
-            <v-card dark color="black darken-1" elevation="5">
-              <v-card-title
-                >{{ application.job.title }} -
-                {{ application.job.contractType }}
-                <v-spacer></v-spacer>
-                <strong>
+<v-row
+		v-for="(application, index) in applications"
+		:key="`job-${index}`"
+		>
+<v-col>
+<v-card dark color="black darken-1" elevation="5">
+<v-card-title
+		>{{ application.job.title }} -
+		{{ application.job.contractType }}
+<v-spacer></v-spacer>
+<strong>
                   Situação:
-                  {{ getStatusLabel(application.status) }}</strong
-                >
-              </v-card-title>
-              <v-card-subtitle
-                >{{ application.job.company }}
-                <span v-if="application.job.officeLocation">
-                  | {{ application.job.officeLocation }}</span
-                ></v-card-subtitle
-              >
-              <v-card-text>
-                <div class="subtitle-1">
-                  Nível:
-                  <strong>{{ application.job.experienceRequired }}</strong>
-                </div>
-                <div class="subtitle-1">
-                  Faixa salarial mínima:
-                  <strong>
+						  {{ getStatusLabel(application.status) }}</strong
+		>
+</v-card-title>
+<v-card-subtitle
+		>{{ application.job.company }}
+<span v-if="application.job.officeLocation">
+		| {{ application.job.officeLocation }}</span
+		></v-card-subtitle
+		>
+<v-card-text>
+<div class="subtitle-1">
+		Nível:
+<strong>{{ application.job.experienceRequired }}</strong>
+</div>
+<div class="subtitle-1">
+		Faixa salarial mínima:
+<strong>
                     {{
-                      format(application.job.salary) +
-                      '/' +
-                      getCompensationTypeLabel(application.job)
-                    }}</strong
-                  >
-                </div>
+							format(application.job.salary) +
+							'/' +
+							getCompensationTypeLabel(application.job)
+							}}</strong
+		>
+</div>
 
-                <div class="my-4">
-                  <p style="white-space: pre-wrap;">
-                    {{ application.job.description }}
-                  </p>
-                </div>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn dark grey @click="openApplication(application.job.uuid)">
-                  Ver candidatura
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-col>
-        </v-row>
-        <v-pagination
-          v-if="this.applications.length > 0"
-          v-model="pageIndex"
-          dark
-          :length="resultSize"
-          @input="nextPage"
-        ></v-pagination>
-      </v-flex>
-    </v-layout>
-  </v-container>
+<div class="my-4">
+<p style="white-space: pre-wrap;">
+		{{ application.job.description }}
+</p>
+</div>
+</v-card-text>
+<v-card-actions>
+<v-spacer></v-spacer>
+<v-btn dark grey @click="openApplication(application.job.uuid)">
+		Ver candidatura
+</v-btn>
+</v-card-actions>
+</v-card>
+</v-col>
+</v-row>
+<v-pagination
+		v-if="this.applications.length > 0"
+		v-model="pageIndex"
+		dark
+		:length="resultSize"
+@input="nextPage"
+		></v-pagination>
+</v-flex>
+</v-layout>
+</v-container>
 </template>
 <script>
 import ErrorAlert from '~/components/ErrorAlert'
 
-const brlFormatter = new Intl.NumberFormat('pt-BR', {
-  style: 'currency',
-  currency: 'BRL',
-  maximumFractionDigits: 2,
-  unitDisplay: 'short',
-})
+		const brlFormatter = new Intl.NumberFormat('pt-BR', {
+		style: 'currency',
+		currency: 'BRL',
+		maximumFractionDigits: 2,
+		unitDisplay: 'short',
+		})
 
-export default {
-  layout: 'search',
-  middleware: 'auth',
-  components: { ErrorAlert },
-  data() {
-    return {
-      expand: false,
-      loading: false,
-      pageIndex: 1,
-      resultSize: 10,
-      applications: [],
-      status: [],
-      filter: {
-        keyWords: [],
-        contractTypes: [],
-        experienceTypes: [],
-      },
-      contractTypes: [
-        { text: 'CLT', value: 'CLT' },
-        { text: 'PJ', value: 'PJ' },
-      ],
-      experienceTypes: [
-        { text: 'Júnior', value: 'JUNIOR' },
-        { text: 'Pleno', value: 'PLENO' },
-        { text: 'Sênior', value: 'SENIOR' },
-        { text: 'Especialista', value: 'ESPECIALISTA' },
-      ],
-      snackbar: false,
-      notification: {
-        title: '',
-        description: '',
-        type: '',
-      },
-    }
-  },
-  mounted() {
-    this.load()
-  },
-  methods: {
-    nextPage(pageNumber) {
-      this.pageIndex = pageNumber
-      this.load()
-      window.scrollTo(0, 0)
-    },
-    search() {
-      this.pageIndex = 1
-      this.load()
-    },
-    async load() {
-      this.loading = true
-      const params = this.filter
-      params['page-index'] = this.pageIndex
-      params['result-size'] = 10
+		export default {
+		layout: 'search',
+		middleware: 'auth',
+		components: { ErrorAlert },
+		data() {
+		return {
+		expand: false,
+		loading: false,
+		pageIndex: 1,
+		resultSize: 10,
+		applications: [],
+		status: [],
+		filter: {
+		keyWords: [],
+		contractTypes: [],
+		experienceTypes: [],
+		},
+		contractTypes: [
+		{ text: 'CLT', value: 'CLT' },
+		{ text: 'PJ', value: 'PJ' },
+		],
+		experienceTypes: [
+		{ text: 'Júnior', value: 'JUNIOR' },
+		{ text: 'Pleno', value: 'PLENO' },
+		{ text: 'Sênior', value: 'SENIOR' },
+		{ text: 'Especialista', value: 'ESPECIALISTA' },
+		],
+		snackbar: false,
+		notification: {
+		title: '',
+		description: '',
+		type: '',
+		},
+		}
+		},
+		mounted() {
+		this.load()
+		},
+		methods: {
+		nextPage(pageNumber) {
+		this.pageIndex = pageNumber
+		this.load()
+		window.scrollTo(0, 0)
+		},
+		search() {
+		this.pageIndex = 1
+		this.load()
+		},
+		async load() {
+		this.loading = true
+		const params = this.filter
+		params['page-index'] = this.pageIndex
+		params['result-size'] = 10
 
-      await this.$api
-        .get('private/my-applications', { params })
-        .then((res) => {
-          this.applications = res.data.content
-          this.loading = false
-          this.resultSize = res.data.totalPages
-        })
-        .catch((err) => {
-          let message = 'Houve um erro inesperado.'
-          if (err.response && err.response.status === 400) {
-            message = err.response.data.message
-          }
+		await this.$api
+		.get('private/my-applications', { params })
+		.then((res) => {
+		this.applications = res.data.content
+		this.loading = false
+		this.resultSize = res.data.totalPages
+		})
+		.catch((err) => {
+		let message = 'Houve um erro inesperado.'
+		if (err.response && err.response.status === 400) {
+		message = err.response.data.message
+		}
 
-          this.notification.title = 'Erro'
-          this.notification.description = message
-          this.notification.type = 'error'
-          this.$refs['message-alert'].showAlert()
-        })
-    },
-    removeKeyWords(item) {
-      this.filter.keyWords.splice(this.filter.keyWords.indexOf(item), 1)
-      this.filter.keyWords = [...this.filter.keyWords]
-    },
-    format(value) {
-      return brlFormatter.format(value)
-    },
-    getCompensationTypeLabel(job) {
-      if (job.compensationType) {
-        switch (job.compensationType) {
-          case 'PER_HOUR':
-            return 'hora'
-          case 'PER_MONTH':
-            return 'mês'
-          case 'PER_YEAR':
-            return 'ano'
-        }
-      } else {
-        if (job.salary >= 0 && job.salary < 600) {
-          return 'hora'
-        }
-        if (job.salary >= 600 && job.salary < 30000) {
-          return 'mês'
-        }
-        if (job.salary >= 30000) {
-          return 'ano'
-        }
-      }
-    },
-    getStatusLabel(status) {
-      switch (status) {
-        case 'APPLIED':
-          return 'Aplicado'
-        case 'REJECTED':
-          return 'Rejeitado'
-        case 'ACCEPTED':
-          return 'Aceito'
-        case 'OFFERED':
-          return 'Oferta'
-        case 'HIRED':
-          return 'Contratado'
-      }
-    },
-    openApplication(uuid) {
-      this.$router.push('/my-applications/' + uuid)
-    },
-  },
-}
+		this.notification.title = 'Erro'
+		this.notification.description = message
+		this.notification.type = 'error'
+		this.$refs['message-alert'].showAlert()
+		})
+		},
+		removeKeyWords(item) {
+		this.filter.keyWords.splice(this.filter.keyWords.indexOf(item), 1)
+		this.filter.keyWords = [...this.filter.keyWords]
+		},
+		format(value) {
+		return brlFormatter.format(value)
+		},
+		getCompensationTypeLabel(job) {
+		if (job.compensationType) {
+		switch (job.compensationType) {
+		case 'PER_HOUR':
+		return 'hora'
+		case 'PER_MONTH':
+		return 'mês'
+		case 'PER_YEAR':
+		return 'ano'
+		}
+		} else {
+		if (job.salary >= 0 && job.salary < 600) {
+		return 'hora'
+		}
+		if (job.salary >= 600 && job.salary < 30000) {
+		return 'mês'
+		}
+		if (job.salary >= 30000) {
+		return 'ano'
+		}
+		}
+		},
+		getStatusLabel(status) {
+		switch (status) {
+		case 'APPLIED':
+		return 'Aplicado'
+		case 'REJECTED':
+		return 'Rejeitado'
+		case 'ACCEPTED':
+		return 'Aceito'
+		case 'OFFERED':
+		return 'Oferta'
+		case 'HIRED':
+		return 'Contratado'
+		}
+		},
+		openApplication(uuid) {
+		this.$router.push('/my-applications/' + uuid)
+		},
+		},
+		}
 </script>
