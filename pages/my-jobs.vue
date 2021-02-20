92891db20efaf68bcdba2efd06a7d44fd503dd9d
Rafael de Agostinho <52942590+Raffsx@users.noreply.github.com>
2021-02-20 19:05:26 -0300

    Rollback my-pages.vue

--- pages/my-jobs.vue
+++ pages/my-jobs.vue
@@ -1,212 +1,205 @@
 <template>
-  <v-container>
-    <v-layout row wrap>
-      <ErrorAlert ref="message-alert" :notification="notification"></ErrorAlert>
-      <v-flex class="align-start">
-        <v-expansion-panels
-          v-model="panel"
-          multiple
-          dark
-          color="black darken-1"
-        >
-          <v-expansion-panel>
-            <v-expansion-panel-header hide-actions dark color="black darken-1">
-              <v-combobox
-                v-model="filter.keyWords"
-                chips
-                clearable
-                hide-details
-                label="Pesquise por palavras chave"
-                prepend-inner-icon="search"
-                multiple
-                solo
-                append-icon=""
-                @click="expand = true"
+  <client-only>
+    <v-container>
+      <v-layout row wrap>
+        <ErrorAlert
+          ref="message-alert"
+          :notification="notification"
+        ></ErrorAlert>
+        <v-flex class="align-start">
+          <v-expansion-panels multiple dark color="black darken-1">
+            <v-expansion-panel>
+              <v-expansion-panel-header
+                hide-actions
+                dark
+                color="black darken-1"
               >
-                <template v-slot:selection="{ attrs, item, select, selected }">
-                  <v-chip
-                    v-bind="attrs"
-                    :input-value="selected"
-                    close
-                    @click="select"
-                    @click:close="removeKeyWords(item)"
+                <v-combobox
+                  v-model="filter.keyWords"
+                  chips
+                  clearable
+                  hide-details
+                  label="Pesquise por palavras chave"
+                  prepend-inner-icon="search"
+                  multiple
+                  solo
+                  append-icon=""
+                  @click="expand = true"
+                >
+                  <template
+                    v-slot:selection="{ attrs, item, select, selected }"
                   >
-                    <strong>{{ item }}</strong
-                    >&nbsp;
-                  </v-chip>
-                </template>
-              </v-combobox>
-            </v-expansion-panel-header>
-            <v-expansion-panel-content dark color="black darken-1">
-              <v-row>
-                <v-col cols="12" sm="6">
-                  <v-select
-                    v-model="filter.contractTypes"
-                    :items="contractTypes"
-                    label="Tipo de contrato"
-                    item-text="text"
-                    item-value="value"
-                    dark
-                    multiple
-                    solo
-                    dense
-                  ></v-select>
-                </v-col>
-                <v-col cols="12" sm="6">
-                  <v-select
-                    v-model="filter.experienceTypes"
-                    :items="experienceTypes"
-                    label="Experiência"
-                    item-text="text"
-                    item-value="value"
-                    dark
-                    multiple
-                    solo
-                    dense
-                  ></v-select>
-                </v-col>
-              </v-row>
-              <v-btn dark block grey @click="search">
-                Pesquisar
-              </v-btn>
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-        </v-expansion-panels>
+                    <v-chip
+                      v-bind="attrs"
+                      :input-value="selected"
+                      close
+                      @click="select"
+                      @click:close="removeKeyWords(item)"
+                    >
+                      <strong>{{ item }}</strong
+                      >&nbsp;
+                    </v-chip>
+                  </template>
+                </v-combobox>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content dark color="black darken-1">
+                <v-row>
+                  <v-col cols="12" sm="6">
+                    <v-select
+                      v-model="filter.contractTypes"
+                      :items="contractTypes"
+                      label="Tipo de contrato"
+                      item-text="text"
+                      item-value="value"
+                      dark
+                      multiple
+                      solo
+                      dense
+                    ></v-select>
+                  </v-col>
+                  <v-col cols="12" sm="6">
+                    <v-select
+                      v-model="filter.experienceTypes"
+                      :items="experienceTypes"
+                      label="Experiência"
+                      item-text="text"
+                      item-value="value"
+                      dark
+                      multiple
+                      solo
+                      dense
+                    ></v-select>
+                  </v-col>
+                </v-row>
+                <v-btn dark block grey @click="search">
+                  Pesquisar
+                </v-btn>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
 
-        <v-divider class="mt-5 mb-5"></v-divider>
+          <v-divider class="mt-5 mb-5"></v-divider>
 
-        <v-row
-          v-if="this.applications.length < 1"
-          align="center"
-          justify="center"
-        >
-          <v-col>
-            <v-card elevation="1">
-              <v-card-text class="text-center">
-                <v-col class="text-center">
-                  <v-row align="center" justify="center">
-                    <h2>
-                      Nenhuma vaga cadastrada, clique no botão abaixo para
-                      cadastrar sua primeira vaga
-                    </h2>
-                  </v-row>
-                  <br />
-                  <v-row align="center" justify="center"
-                    ><v-btn dark to="/job">Cadastrar uma vaga</v-btn></v-row
-                  >
-                </v-col>
-              </v-card-text>
-            </v-card>
-          </v-col>
-        </v-row>
+          <v-row v-if="this.jobs.length < 1" align="center" justify="center">
+            <v-col>
+              <v-card elevation="1">
+                <v-card-text class="text-center">
+                  <v-col class="text-center">
+                    <v-row align="center" justify="center">
+                      <h2>
+                        Nenhuma vaga cadastrada, clique no botão abaixo para
+                        cadastrar sua primeira vaga
+                      </h2>
+                    </v-row>
+                    <br />
+                    <v-row align="center" justify="center"
+                      ><v-btn dark to="/job">Cadastrar uma vaga</v-btn></v-row
+                    >
+                  </v-col>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
 
-        <v-pagination
-          v-if="this.applications.length > 0"
-          v-model="pageIndex"
-          dark
-          :length="resultSize"
-          @input="nextPage"
-        ></v-pagination>
-
-        <div v-if="loading" class="text-center ma-5">
-          <v-progress-circular
-            :size="100"
-            :width="10"
-            indeterminate
+          <v-pagination
+            v-if="this.jobs.length > 0"
+            v-model="pageIndex"
             dark
-          ></v-progress-circular>
-        </div>
+            :length="resultSize"
+            @input="nextPage"
+          ></v-pagination>
 
-        <v-row
-          v-for="(application, index) in applications"
-          :key="`job-${index}`"
-        >
-          <v-col>
-            <v-card
+          <div v-if="this.loading" class="text-center ma-5">
+            <v-progress-circular
+              :size="100"
+              :width="10"
+              indeterminate
               dark
-              :color="
-                application.job.jobStatus === 'CLOSED'
-                  ? 'grey darken-1'
-                  : 'black darken-1'
-              "
-              elevation="5"
-            >
-              <v-card-title
-                >{{ application.job.title }} -
-                {{ application.job.contractType }}</v-card-title
-              >
-              <v-card-subtitle
-                >{{ application.job.company }}
-                <span v-if="application.job.officeLocation">
-                  | {{ application.job.officeLocation }}</span
-                ></v-card-subtitle
-              >
+            ></v-progress-circular>
+          </div>
 
-              <v-card-text>
-                <div class="subtitle-1">
-                  Nível:
-                  <strong>{{ application.job.experienceRequired }}</strong>
-                </div>
-                <div class="subtitle-1">
-                  Faixa salarial mínima:
-                  <strong>
-                    {{
-                      format(application.job.salary) +
-                      '/' +
-                      getCompensationTypeLabel(application.job)
-                    }}</strong
+          <v-row v-for="(job, index) in jobs" :key="`job-${index}`">
+            <v-col>
+              <v-card
+                dark
+                :color="
+                  job.jobStatus === 'CLOSED'
+                    ? 'grey darken-1'
+                    : 'black darken-1'
+                "
+                elevation="5"
+              >
+                <v-card-title
+                  >{{ job.title }} - {{ job.contractType }}</v-card-title
+                >
+                <v-card-subtitle
+                  >{{ job.company }}
+                  <span v-if="job.officeLocation">
+                    | {{ job.officeLocation }}</span
+                  ></v-card-subtitle
+                >
+
+                <v-card-text>
+                  <div class="subtitle-1">
+                    Nível: <strong>{{ job.experienceRequired }}</strong>
+                  </div>
+                  <div class="subtitle-1">
+                    Faixa salarial mínima:
+                    <strong>
+                      {{
+                        format(job.salary) + '/' + getCompensationTypeLabel(job)
+                      }}</strong
+                    >
+                  </div>
+
+                  <div class="my-4">
+                    <p style="white-space: pre-wrap;">{{ job.description }}</p>
+                  </div>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    dark
+                    grey
+                    :disabled="job.jobStatus === 'CLOSED'"
+                    @click="edit(job.uuid)"
                   >
-                </div>
-
-                <div class="my-4">
-                  <p style="white-space: pre-wrap;">
-                    {{ application.job.description }}
-                  </p>
-                </div>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn
-                  dark
-                  grey
-                  :disabled="application.job.jobStatus === 'CLOSED'"
-                  @click="edit(application.job.uuid)"
-                >
-                  Editar
-                </v-btn>
-                <v-btn dark grey @click="openCandidates(application.job.uuid)">
-                  Ver candidatos
-                </v-btn>
-                <v-btn
-                  v-if="application.job.jobStatus !== 'CLOSED'"
-                  dark
-                  grey
-                  @click="closeJob(application.job.uuid)"
-                >
-                  Fechar vaga
-                </v-btn>
-                <v-btn
-                  v-if="application.job.jobStatus === 'CLOSED'"
-                  dark
-                  grey
-                  @click="reopenJob(application.job.uuid)"
-                >
-                  Reabrir vaga
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-col>
-        </v-row>
-        <v-pagination
-          v-if="this.applications.length > 0"
-          v-model="pageIndex"
-          dark
-          :length="resultSize"
-          @input="nextPage"
-        ></v-pagination>
-      </v-flex>
-    </v-layout>
-  </v-container>
+                    Editar
+                  </v-btn>
+                  <v-btn dark grey @click="openCandidates(job.uuid)">
+                    Ver candidatos
+                  </v-btn>
+                  <v-btn
+                    v-if="job.jobStatus !== 'CLOSED'"
+                    dark
+                    grey
+                    @click="closeJob(job.uuid)"
+                  >
+                    Fechar vaga
+                  </v-btn>
+                  <v-btn
+                    v-if="job.jobStatus === 'CLOSED'"
+                    dark
+                    grey
+                    @click="reopenJob(job.uuid)"
+                  >
+                    Reabrir vaga
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
+          <v-pagination
+            v-if="this.jobs.length > 0"
+            v-model="pageIndex"
+            dark
+            :length="resultSize"
+            @input="nextPage"
+          ></v-pagination>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </client-only>
 </template>
 <script>
 import ErrorAlert from '~/components/ErrorAlert'
@@ -228,7 +221,7 @@ export default {
       loading: false,
       pageIndex: 1,
       resultSize: 10,
-      applications: [],
+      jobs: [],
       filter: {
         keyWords: [],
         contractTypes: [],
@@ -273,9 +266,9 @@ export default {
       params['result-size'] = 10
 
       await this.$api
-        .get(`private/my-applications/findAllByFilterAndTenant`, { params })
+        .get(`private/my-jobs`, { params })
         .then((res) => {
-          this.applications = res.data.content
+          this.jobs = res.data.content
           this.loading = false
           this.resultSize = res.data.totalPages
         })
@@ -290,7 +283,6 @@ export default {
           this.notification.type = 'error'
           this.$refs['message-alert'].showAlert()
         })
-      console.log(this.applications)
     },
     removeKeyWords(item) {
       this.filter.keyWords.splice(this.filter.keyWords.indexOf(item), 1)
